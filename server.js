c5f4c08be4faeaa79da1c0d6e0b5077121f0b406
doobuhm-code <doobu.hm@gmail.com>
2026-07-25 08:04:11 +0000

    fix: make server PORT dynamic to support Railway environment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const runLottoCollector = require('./collector_lotto');
 const runPensionCollector = require('./collector_pension');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
